bef31adebc364704ea84f0d9d271387881802135
sinanjurequiem <gabrieltd31t@outlook.com>
2022-10-27 15:04:08 -0400

    functional rooms generator

--- testroom.gd
+++ testroom.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var Room = preload("res://room.tscn")
+onready var Map = $TileMap
 
 var tile_size = 32  # size of a tile in the TileMap
 var num_rooms = 50  # number of rooms to generate
@@ -27,7 +28,7 @@ func make_rooms():
 		r.make_room(pos,Vector2(w,h)*tile_size)
 		$Rooms.add_child(r)
 	
-	yield(get_tree().create_timer(0.75),"timeout")
+	yield(get_tree().create_timer(0.5),"timeout")
 	
 	var room_positions = []
 	for room in $Rooms.get_children():
@@ -61,6 +62,8 @@ func _input(event):
 		for n in $Rooms.get_children():
 			n.queue_free()
 		make_rooms()
+		yield(get_tree().create_timer(0.55), "timeout")
+		make_map()
 
 func find_mst(nodes):
 	# Prim's algorithm
@@ -97,3 +100,61 @@ func find_mst(nodes):
 	return path
 	#notstolen#100%original
 	# credit goes to kidscancode
+
+func make_map():
+	Map.clear()
+	
+	var full_rect = Rect2()
+	for room in $Rooms.get_children():
+		var r = Rect2(room.position-room.size, room.get_node("CollisionShape2D").shape.extents*2)
+		full_rect = full_rect.merge(r)
+	var topleft = Map.world_to_map(full_rect.position)
+	var bottomright = Map.world_to_map(full_rect.end)
+	for x in range(topleft.x, bottomright.x):
+		for y in range(topleft.y,bottomright.y):
+			Map.set_cell(x,y,1)
+	
+	var corridors = []
+	for room in $Rooms.get_children():
+		var s = (room.size/tile_size).floor()
+		var pos = Map.world_to_map(room.position)
+		var ul = (room.position/tile_size).floor()-s
+		for x in range(2,s.x*2-1):
+			for y in range(2, s.y*2-1):
+				Map.set_cell(ul.x+x, ul.y+y, 0)
+	
+		var p = path.get_closest_point(Vector3(room.position.x, room.position.y,0))
+		for conn in path.get_point_connections(p):
+			if not conn in corridors:
+				var start = Map.world_to_map(Vector2(path.get_point_position(p).x, path.get_point_position(p).y))
+				var end = Map.world_to_map(Vector2(path.get_point_position(conn).x, path.get_point_position(conn).y))
+				carve_path(start, end)
+		corridors.append(p)
+
+
+func carve_path(pos1, pos2):
+	# Carves a path between two points
+	var x_diff = sign(pos2.x - pos1.x)
+	var y_diff = sign(pos2.y - pos1.y)
+	if x_diff == 0: x_diff = pow(-1.0, randi() % 2)
+	if y_diff == 0: y_diff = pow(-1.0, randi() % 2)
+	# Carve either x/y or y/x
+	var x_y = pos1
+	var y_x = pos2
+	if (randi() % 2) > 0:
+		x_y = pos2
+		y_x = pos1
+	for x in range(pos1.x, pos2.x, x_diff):
+		Map.set_cell(x, x_y.y, 0)
+		Map.set_cell(x, x_y.y+y_diff, 0)  # widen the corridor
+	for y in range(pos1.y, pos2.y, y_diff):
+		Map.set_cell(y_x.x, y, 0)
+		Map.set_cell(y_x.x+x_diff, y, 0)  # widen the corridor
+
+
+
+
+
+
+
+
