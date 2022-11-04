1027be8d90e5f1d14a0dc9dec9a46b7c3ab9c4db
sinanjurequiem <gabrieltd31t@outlook.com>
2022-11-04 16:18:30 -0400

    added sword mechanics (unfixed)

--- Characters/Player.gd
+++ Characters/Player.gd
@@ -1,6 +1,10 @@
 extends Character
 
 onready var animated_sprite = $"AnimatedSprite"
+onready var weapon = $"Weapon"
+onready var weapon_animation_player = $"Weapon/Sword/AnimationPlayer" #fix this later to work with other weapons
+
+signal main_attack
 
 func _process(delta):
 	var mouse_direction: Vector2 = (get_global_mouse_position() - global_position).normalized()
@@ -9,6 +13,12 @@ func _process(delta):
 		animated_sprite.flip_h = false
 	elif mouse_direction.x < 0 and not animated_sprite.flip_h:
 		animated_sprite.flip_h = true
+	
+	weapon.rotation = mouse_direction.angle()
+	if weapon.scale.y == 1 and mouse_direction.x < 0:
+		weapon.scale.y = -1
+	elif weapon.scale.y == -1 and mouse_direction.x > 0:
+		weapon.scale.y = 1
 	
 	get_input()
 
@@ -22,4 +32,9 @@ func get_input():
 		direction+= Vector2.RIGHT
 	if Input.is_action_pressed("ui_left"):
 		direction+= Vector2.LEFT
+	if Input.is_action_pressed("ui_attack_main"):
+		emit_signal("main_attack")
 
+
+func _on_Weapon_attack():
+	pass # Replace with function body.
---
+++ Characters/Weapon.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+#idk what to put here
+
+
+
+
+func _on_Player_main_attack():
+	pass # Replace with function body.
